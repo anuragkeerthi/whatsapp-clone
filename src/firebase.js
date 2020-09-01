93216c98aee5fecd3588a1af2e5ac15f35c36bfb
Anurag Keerthi <anuragkeerthi@gmail.com>
2020-09-01 23:20:33 +0530

    Added Sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBX-EMm5zw0x-nnGIhGN8VUTuRKPQW4FEY",
+    authDomain: "whatsapp-clone-8390f.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-8390f.firebaseio.com",
+    projectId: "whatsapp-clone-8390f",
+    storageBucket: "whatsapp-clone-8390f.appspot.com",
+    messagingSenderId: "735448044472",
+    appId: "1:735448044472:web:ca195a6b8c08913659f54d",
+    measurementId: "G-L1YMJT4DWB"
+  };
