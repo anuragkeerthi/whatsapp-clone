a71d1c5c36344849bab8ef3989efba37c8f97531
Anurag Keerthi <anuragkeerthi@gmail.com>
2020-09-02 23:11:32 +0530

    Firebase

--- src/Chat.js
+++ src/Chat.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import { Avatar, IconButton } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
 import "./Chat.css";
+import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
+    const [seed, setSeed] = useState("");
+    const [input, setInput] = useState("");
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log("You typed >>>", input);
+
+        setInput("");
+    };
+
     return (
         <div className="chat">
 
             <div className = "chat__header">
-
+                <Avatar src = {`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className = "chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last Seen at ...</p>
+                </div>
+                <div className="chat__headerRight">
+                <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
             </div>
             <div className = "chat__body">
-
+                <p className ={`chat__message ${true && 'chat__reciever'}`}>
+                <span className="chat__name">Anurag Keerthi</span>
+                Hi! there..
+                <span className="chat__timestamp">01:43pm</span>
+                </p>
             </div>
             <div className = "chat__footer">
-                
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage} type="submit"> Send a Message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBX-EMm5zw0x-nnGIhGN8VUTuRKPQW4FEY",
     authDomain: "whatsapp-clone-8390f.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:735448044472:web:ca195a6b8c08913659f54d",
     measurementId: "G-L1YMJT4DWB"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  
+  export { auth, provider};
+  export default db;
